49bcbe7fd7fdd01d7b6561dba1b534270f801b01
takumitakeno <matchball.shine@gmail.com>
2020-01-30 11:33:54 +0900

    [Update 5-9]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,10 @@
+class BooksController < ApplicationController
+	def index
+		@book =Book.all
+		
+	end
+
+	def 
+		
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  # get 'users/show'resources :usersと重複するため削除する。
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:show]
+
+  resources :books
+
+  
 end
+
