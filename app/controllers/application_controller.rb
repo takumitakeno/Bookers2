ebe9f9d1847286bb117240e6c5c1c524a595d8fd
takumitakeno <matchball.shine@gmail.com>
2020-02-02 20:23:09 +0900

    [Update5-9]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
-  
+  def after_sign_in_path_for(resourse)
+  	  user_path(resourse)
+  	
+  end
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
   def configure_permitted_parameters
